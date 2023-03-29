90d8308ee78a1fba81a99a6f6c1bf3f561dd2ddc
Kyle Skinner <kyleskin@gmail.com>
2023-03-29 14:28:32 -0600

    ShouldThrowValidationExceptionWhenPetBirthdateIsInvalidAndLogItAsync -> FAIL

--- FurRealCute.Web.Api.Tests.Unit/Services/Pets/PetServiceTests.Validations.Add.cs
+++ FurRealCute.Web.Api.Tests.Unit/Services/Pets/PetServiceTests.Validations.Add.cs
@@ -110,4 +110,35 @@ public partial class PetServiceTests
         _loggingBrokerMock.VerifyNoOtherCalls();
         _storageBrokerMock.VerifyNoOtherCalls();
     }
+
+    [Fact]
+    public async Task ShouldThrowValidationExceptionWhenPetBirthdateIsInvalidAndLogItAsync()
+    {
+        // Arrange
+        DateTimeOffset dateTime = GetRandomDateTime();
+        Pet randomPet = CreateRandomPet(dateTime);
+        Pet inputPet = randomPet;
+        inputPet.Birthdate = DateTimeOffset.Now.AddDays(1);
+
+        InvalidPetException invalidPetException = new(
+            parameterName: nameof(Pet.Birthdate),
+            parameterValue: inputPet.Birthdate);
+
+        PetValidationException expectedPetValidationException = new(invalidPetException);
+        
+        // Act
+        ValueTask<Pet> createPetTask = _petService.CreatePetAsync(inputPet);
+        
+        // Assert
+        await Assert.ThrowsAsync<PetValidationException>(() =>
+            createPetTask.AsTask());
+        
+        _loggingBrokerMock.Verify(broker =>
+            broker.LogError(It.Is(SameExceptionAs(expectedPetValidationException))), 
+            Times.Once);
+        
+        _dateTimeBrokerMock.VerifyNoOtherCalls();
+        _loggingBrokerMock.VerifyNoOtherCalls();
+        _storageBrokerMock.VerifyNoOtherCalls();
+    }
 }
