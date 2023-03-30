7395160822f51d51592ef88bee06b2005a06e7c1
Kyle Skinner <kyleskin@gmail.com>
2023-03-30 14:37:24 -0600

    ShouldThrowServiceExceptionOnRetrieveByIdWhenExceptionOccursAndLogItAsync -> PASS

--- FurRealCute.Web.Api.Tests.Unit/Services/Pets/PetServiceTests.Exceptions.RetrieveById.cs
+++ FurRealCute.Web.Api.Tests.Unit/Services/Pets/PetServiceTests.Exceptions.RetrieveById.cs
@@ -75,4 +75,39 @@ public partial class PetServiceTests
         _loggingBrokerMock.VerifyNoOtherCalls();
         _storageBrokerMock.VerifyNoOtherCalls();
     }
+
+    [Fact]
+    public async Task ShouldThrowServiceExceptionOnRetrieveByIdWhenExceptionOccursAndLogItAsync()
+    {
+        // Arrange
+        Guid randomId = Guid.NewGuid();
+        Guid inputId = randomId;
+        Exception serviceException = new();
+
+        FailedPetServiceException failedPetServiceException = new(serviceException);
+        PetServiceException expectedPetServiceException = new(failedPetServiceException);
+
+        _storageBrokerMock.Setup(broker =>
+                broker.SelectPetByIdAsync(inputId))
+            .ThrowsAsync(failedPetServiceException);
+        
+        // Act
+        ValueTask<Pet?> retrievePetTask = _petService.RetrievePetByIdAsync(inputId);
+        
+        // Arrange
+        await Assert.ThrowsAsync<PetServiceException>(() =>
+            retrievePetTask.AsTask());
+        
+        _loggingBrokerMock.Verify(broker =>
+            broker.LogError(It.Is(SameExceptionAs(expectedPetServiceException))),
+            Times.Once);
+
+        _storageBrokerMock.Verify(broker =>
+                broker.SelectPetByIdAsync(inputId),
+            Times.Once);
+        
+        _dateTimeBrokerMock.VerifyNoOtherCalls();
+        _loggingBrokerMock.VerifyNoOtherCalls();
+        _storageBrokerMock.VerifyNoOtherCalls();
+    }
 }
