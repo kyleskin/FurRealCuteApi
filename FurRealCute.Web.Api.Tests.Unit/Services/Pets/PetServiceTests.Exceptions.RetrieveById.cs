414701283cc895ccc41b64491deb7d793088e9bd
Kyle Skinner <kyleskin@gmail.com>
2023-03-30 14:30:03 -0600

    ShouldThrowDependencyExceptionOnRetrieveByIdWhenDbExceptionOccursAndLogItAsync -> PASS

--- FurRealCute.Web.Api.Tests.Unit/Services/Pets/PetServiceTests.Exceptions.RetrieveById.cs
+++ FurRealCute.Web.Api.Tests.Unit/Services/Pets/PetServiceTests.Exceptions.RetrieveById.cs
@@ -1,5 +1,6 @@
 using FurRealCute.Web.Api.Models.Pets;
 using FurRealCute.Web.Api.Models.Pets.Exceptions;
+using Microsoft.EntityFrameworkCore;
 using Moq;
 using Npgsql;
 
@@ -31,6 +32,39 @@ public partial class PetServiceTests
 
         _loggingBrokerMock.Verify(broker =>
                 broker.LogCritical(It.Is(SameExceptionAs(expectedPetDependencyException))),
+            Times.Once);
+        
+        _storageBrokerMock.Verify(broker =>
+            broker.SelectPetByIdAsync(inputId),
+            Times.Once);
+        
+        _dateTimeBrokerMock.VerifyNoOtherCalls();
+        _loggingBrokerMock.VerifyNoOtherCalls();
+        _storageBrokerMock.VerifyNoOtherCalls();
+    }
+
+    [Fact]
+    public async Task ShouldThrowDependencyExceptionOnRetrieveByIdWhenDbExceptionOccursAndLogItAsync()
+    {
+        // Arrange
+        Guid randomId = Guid.NewGuid();
+        Guid inputId = randomId;
+        DbUpdateException databaseUpdateException = new();
+
+        PetDependencyException expectedPetDependencyException = new(databaseUpdateException);
+
+        _storageBrokerMock.Setup(broker =>
+                broker.SelectPetByIdAsync(inputId))
+            .ThrowsAsync(databaseUpdateException);
+        
+        // Act
+        ValueTask<Pet?> retrievePetTask = _petService.RetrievePetByIdAsync(inputId);
+        
+        // Assert
+        await Assert.ThrowsAsync<PetDependencyException>(() => retrievePetTask.AsTask());
+        
+        _loggingBrokerMock.Verify(broker =>
+            broker.LogError(It.Is(SameExceptionAs(expectedPetDependencyException))),
             Times.Once);
         
         _storageBrokerMock.Verify(broker =>
