da169ebe6de9734dd9561dafc2157aca3edb9dd5
Kyle Skinner <kyleskin@gmail.com>
2023-03-29 19:57:36 -0600

    remove failing test

--- FurRealCute.Web.Api.Tests.Unit/Services/Pets/PetServiceTests.Logic.RetrieveAll.cs
+++ FurRealCute.Web.Api.Tests.Unit/Services/Pets/PetServiceTests.Logic.RetrieveAll.cs
@@ -4,9 +4,4 @@ namespace FurRealCute.Web.Api.Tests.Unit.Services.Pets;
 
 public partial class PetServiceTests
 {
-    [Fact]
-    public void ShouldFail()
-    {
-        Assert.False(true);
-    }
 }
