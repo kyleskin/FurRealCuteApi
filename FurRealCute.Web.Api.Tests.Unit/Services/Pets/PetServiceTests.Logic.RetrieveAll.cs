f0ea700db0eb1a85e72a9fd2aafb1a85266c172c
Kyle Skinner <kyleskin@gmail.com>
2023-03-29 19:52:07 -0600

    pushing failing test to try out GitHub actions/badge

--- FurRealCute.Web.Api.Tests.Unit/Services/Pets/PetServiceTests.Logic.RetrieveAll.cs
+++ FurRealCute.Web.Api.Tests.Unit/Services/Pets/PetServiceTests.Logic.RetrieveAll.cs
@@ -4,5 +4,9 @@ namespace FurRealCute.Web.Api.Tests.Unit.Services.Pets;
 
 public partial class PetServiceTests
 {
-    
+    [Fact]
+    public void ShouldFail()
+    {
+        Assert.False(true);
+    }
 }
