aa5fed6fe1ab9c818435cf86f5719632d34a1166
Kyle Skinner <kyleskin@gmail.com>
2023-03-30 10:37:42 -0600

    ShouldThrowDependencyExceptionOnAddWhenDbUpdateExceptionOccursAndLogItAsync -> FAIL

--- FurRealCute.Web.Api.Tests.Unit/Services/Pets/PetServiceTests.Exceptions.Add.cs
+++ FurRealCute.Web.Api.Tests.Unit/Services/Pets/PetServiceTests.Exceptions.Add.cs
@@ -1,6 +1,7 @@
 using FluentAssertions;
 using FurRealCute.Web.Api.Models.Pets;
 using FurRealCute.Web.Api.Models.Pets.Exceptions;
+using Microsoft.EntityFrameworkCore;
 using Moq;
 using Npgsql;
 
@@ -51,5 +52,48 @@ public partial class PetServiceTests
         _loggingBrokerMock.VerifyNoOtherCalls();
         _storageBrokerMock.VerifyNoOtherCalls();
         
+    }
+
+    [Fact]
+    public async Task ShouldThrowDependencyExceptionOnAddWhenDbUpdateExceptionOccursAndLogItAsync()
+    {
+        // Arrange
+        DateTimeOffset dateTime = GetRandomDateTime();
+        Pet randomPet = CreateRandomPet(dateTime);
+        Pet inputPet = randomPet;
+        DbUpdateException databaseUpdateException = new();
+
+        PetDependencyException expectedPetDependencyException = new(databaseUpdateException);
+
+        _dateTimeBrokerMock.Setup(broker =>
+                broker.GetCurrentDateTime())
+            .Returns(dateTime);
+
+        _storageBrokerMock.Setup(broker =>
+                broker.InsertPetAsync(inputPet))
+            .ThrowsAsync(databaseUpdateException);
+        
+        // Act
+        ValueTask<Pet> createPetTask = _petService.CreatePetAsync(inputPet);
+        
+        // Assert
+        await Assert.ThrowsAsync<PetDependencyException>(() =>
+            createPetTask.AsTask());
+        
+        _loggingBrokerMock.Verify(broker =>
+            broker.LogError(It.Is(SameExceptionAs(expectedPetDependencyException))), 
+            Times.Once);
+        
+        _storageBrokerMock.Verify(broker =>
+                broker.InsertPetAsync(inputPet),
+            Times.Once);
+        
+        _dateTimeBrokerMock.Verify(broker =>
+            broker.GetCurrentDateTime(),
+            Times.Once);
+        
+        _dateTimeBrokerMock.VerifyNoOtherCalls();
+        _loggingBrokerMock.VerifyNoOtherCalls();
+        _storageBrokerMock.VerifyNoOtherCalls();
     }
 }
