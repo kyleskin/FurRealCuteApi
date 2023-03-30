30953aa1dfd933aa4ffb9b68c3f723e080063140
Kyle Skinner <kyleskin@gmail.com>
2023-03-29 20:43:07 -0600

    ShouldRetrieveAllPets -> FAIL

--- FurRealCute.Web.Api.Tests.Unit/Services/Pets/PetServiceTests.Logic.RetrieveAll.cs
+++ FurRealCute.Web.Api.Tests.Unit/Services/Pets/PetServiceTests.Logic.RetrieveAll.cs
@@ -1,7 +1,37 @@
-﻿using FurRealCute.Web.Api.Services.Pets;
+﻿using FluentAssertions;
+using FurRealCute.Web.Api.Models.Pets;
+using FurRealCute.Web.Api.Services.Pets;
+using Moq;
 
 namespace FurRealCute.Web.Api.Tests.Unit.Services.Pets;
 
 public partial class PetServiceTests
 {
+    [Fact]
+    public async Task ShouldRetrieveAllPets()
+    {
+        // Arrange
+        DateTimeOffset dateTime = GetRandomDateTime();
+        IQueryable<Pet> randomPets = CreateRandomPets();
+        IQueryable<Pet> storagePets = randomPets;
+        IQueryable<Pet> expectedPets = storagePets;
+
+        _storageBrokerMock.Setup(broker =>
+                broker.SelectAllPets().Result)
+            .Returns(storagePets);
+        
+        // Act
+        IQueryable<Pet> actualPets = await _petService.RetrieveAllPetsAsync();
+        
+        // Assert
+        actualPets.Should().BeEquivalentTo(expectedPets);
+        
+        _storageBrokerMock.Verify(broker =>
+            broker.SelectAllPets(),
+            Times.Once);
+        
+        _dateTimeBrokerMock.VerifyNoOtherCalls();
+        _storageBrokerMock.VerifyNoOtherCalls();
+        _loggingBrokerMock.VerifyNoOtherCalls();
+    }
 }
