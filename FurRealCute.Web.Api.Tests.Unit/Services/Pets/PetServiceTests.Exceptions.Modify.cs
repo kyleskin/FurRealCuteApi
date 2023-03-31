ac6fb2bd3caa08446bc9cb5967cf8c85eb99fa12
Kyle Skinner <kyleskin@gmail.com>
2023-03-31 10:36:49 -0600

    ShouldThrowDependencyExceptionOnModifyIfDbUpdateExceptionOccursAndLogItAsync -> PASS

--- FurRealCute.Web.Api.Tests.Unit/Services/Pets/PetServiceTests.Exceptions.Modify.cs
+++ FurRealCute.Web.Api.Tests.Unit/Services/Pets/PetServiceTests.Exceptions.Modify.cs
@@ -1,5 +1,6 @@
 using FurRealCute.Web.Api.Models.Pets;
 using FurRealCute.Web.Api.Models.Pets.Exceptions;
+using Microsoft.EntityFrameworkCore;
 using Moq;
 using Npgsql;
 
@@ -46,6 +47,52 @@ public partial class PetServiceTests
         
         _loggingBrokerMock.Verify(broker =>
             broker.LogCritical(It.Is(SameExceptionAs(expectedDependencyException))),
+            Times.Once);
+        
+        _dateTimeBrokerMock.VerifyNoOtherCalls();
+        _loggingBrokerMock.VerifyNoOtherCalls();
+        _storageBrokerMock.VerifyNoOtherCalls();
+    }
+
+    [Fact]
+    public async Task ShouldThrowDependencyExceptionOnModifyIfDbUpdateExceptionOccursAndLogItAsync()
+    {
+        // Arrange
+        int randomNumber = GetNegativeRandomNumber();
+        int randomMinutes = randomNumber;
+        DateTimeOffset dateTime = GetRandomDateTime();
+        Pet randomPet = CreateRandomPet(dateTime);
+        Pet inputPet = randomPet;
+        Guid petId = inputPet.Id;
+        inputPet.CreatedDate = inputPet.CreatedDate.AddMinutes(randomMinutes);
+
+        DbUpdateException dbUpdateException = new();
+        PetDependencyException expectedDependencyException = new(dbUpdateException);
+        
+        _storageBrokerMock.Setup(broker =>
+                broker.SelectPetByIdAsync(petId))
+            .ThrowsAsync(dbUpdateException);
+
+        _dateTimeBrokerMock.Setup(broker =>
+                broker.GetCurrentDateTime())
+            .Returns(dateTime);
+        
+        // Act
+        ValueTask<Pet?> modifyPetTask = _petService.ModifyPetAsync(inputPet);
+        
+        // Assert
+        await Assert.ThrowsAsync<PetDependencyException>(() => modifyPetTask.AsTask());
+        
+        _dateTimeBrokerMock.Verify(broker =>
+                broker.GetCurrentDateTime(),
+            Times.Once);
+        
+        _storageBrokerMock.Verify(broker =>
+                broker.SelectPetByIdAsync(inputPet.Id),
+            Times.Once);
+        
+        _loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(expectedDependencyException))),
             Times.Once);
         
         _dateTimeBrokerMock.VerifyNoOtherCalls();
