baffa05554082b65d4a7cfeac37c5343ecec7e35
Kyle Skinner <kyleskin@gmail.com>
2023-03-31 18:17:34 -0600

    ShouldDeletePetAsync -> FAIL

---
+++ FurRealCute.Web.Api.Tests.Unit/Services/Pets/PetServiceTests.Logic.Delete.cs
@@ -0,0 +1,45 @@
+﻿using FluentAssertions;
+using FurRealCute.Web.Api.Models.Pets;
+using Moq;
+
+namespace FurRealCute.Web.Api.Tests.Unit.Services.Pets;
+
+public partial class PetServiceTests
+{
+    [Fact]
+    public async Task ShouldDeletePetAsync()
+    {
+        // Arrange
+        DateTimeOffset dateTime = GetRandomDateTime();
+        Pet randomPet = CreateRandomPet(dateTime);
+        Pet storagePet = randomPet;
+        Guid petId = storagePet.Id;
+        Pet expectedPet = storagePet;
+
+        _storageBrokerMock.Setup(broker =>
+                broker.SelectPetByIdAsync(petId))
+            .ReturnsAsync(storagePet);
+
+        _storageBrokerMock.Setup(broker =>
+                broker.DeletePetAsync(storagePet))
+            .ReturnsAsync(expectedPet);
+        
+        // Act
+        Pet actualPet = _petService.RemovePetByIdAsync(petId);
+        
+        // Assert
+        actualPet.Should().BeEquivalentTo(expectedPet);
+        
+        _storageBrokerMock.Verify(broker =>
+            broker.SelectPetByIdAsync(petId),
+            Times.Once);
+        
+        _storageBrokerMock.Verify(broker =>
+            broker.DeletePetAsync(storagePet),
+            Times.Once);
+        
+        _dateTimeBrokerMock.VerifyNoOtherCalls();
+        _loggingBrokerMock.VerifyNoOtherCalls();
+        _storageBrokerMock.VerifyNoOtherCalls();
+    }
+}
