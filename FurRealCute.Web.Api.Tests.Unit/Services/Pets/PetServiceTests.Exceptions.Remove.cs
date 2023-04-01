b322df8ec40f15a9ffad2fac6aae3e802531a98d
Kyle Skinner <kyleskin@gmail.com>
2023-04-01 09:22:29 -0600

    ShouldThrowDependencyExceptionOnRemoveWhenDbExceptionOccursAndLogItAsync -> PASS

--- FurRealCute.Web.Api.Tests.Unit/Services/Pets/PetServiceTests.Exceptions.Remove.cs
+++ FurRealCute.Web.Api.Tests.Unit/Services/Pets/PetServiceTests.Exceptions.Remove.cs
@@ -1,6 +1,7 @@
 using System.Runtime.CompilerServices;
 using FurRealCute.Web.Api.Models.Pets;
 using FurRealCute.Web.Api.Models.Pets.Exceptions;
+using Microsoft.EntityFrameworkCore;
 using Moq;
 using Npgsql;
 
@@ -30,6 +31,39 @@ public partial class PetServiceTests
         
         _loggingBrokerMock.Verify(broker =>
             broker.LogCritical(It.Is(SameExceptionAs(expectedPetDependencyException))),
+            Times.Once);
+        
+        _storageBrokerMock.Verify(broker =>
+            broker.SelectPetByIdAsync(inputId),
+            Times.Once);
+        
+        _dateTimeBrokerMock.VerifyNoOtherCalls();
+        _loggingBrokerMock.VerifyNoOtherCalls();
+        _storageBrokerMock.VerifyNoOtherCalls();
+    }
+
+    [Fact]
+    public async Task ShouldThrowDependencyExceptionOnRemoveWhenDbExceptionOccursAndLogItAsync()
+    {
+        // Arrange
+        Guid randomId = Guid.NewGuid();
+        Guid inputId = randomId;
+
+        DbUpdateException dbUpdateException = new();
+        PetDependencyException expectedPetDependencyException = new(dbUpdateException);
+
+        _storageBrokerMock.Setup(broker =>
+                broker.SelectPetByIdAsync(inputId))
+            .ThrowsAsync(dbUpdateException);
+        
+        // Act
+        ValueTask<Pet> removePetTask = _petService.RemovePetByIdAsync(inputId);
+        
+        // Assert
+        await Assert.ThrowsAsync<PetDependencyException>(() => removePetTask.AsTask());
+        
+        _loggingBrokerMock.Verify(broker =>
+            broker.LogError(It.Is(SameExceptionAs(expectedPetDependencyException))),
             Times.Once);
         
         _storageBrokerMock.Verify(broker =>
