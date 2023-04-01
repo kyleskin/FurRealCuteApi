9d081be0552ea821eb34efc451f14bd7818165e2
Kyle Skinner <kyleskin@gmail.com>
2023-04-01 07:59:31 -0600

    ShouldThrowValidationExceptionOnRemoveWhenStoragePetIsNullAndLogItAsync -> FAIL

--- FurRealCute.Web.Api.Tests.Unit/Services/Pets/PetServiceTests.Validations.Remove.cs
+++ FurRealCute.Web.Api.Tests.Unit/Services/Pets/PetServiceTests.Validations.Remove.cs
@@ -32,4 +32,39 @@ public partial class PetServiceTests
         _loggingBrokerMock.VerifyNoOtherCalls();
         _storageBrokerMock.VerifyNoOtherCalls();
     }
+
+    [Fact]
+    public async Task ShouldThrowValidationExceptionOnRemoveWhenStoragePetIsNullAndLogItAsync()
+    {
+        // Arrange
+        Guid randomId = Guid.NewGuid();
+        Guid inputId = randomId;
+        Pet? invalidStoragePet = null;
+
+        NotFoundPetException notFoundPetException = new(inputId);
+
+        PetValidationException expectedPetValidationException = new(notFoundPetException);
+
+        _storageBrokerMock.Setup(broker =>
+                broker.SelectPetByIdAsync(inputId))
+            .ReturnsAsync(invalidStoragePet);
+        
+        // Act
+        ValueTask<Pet> deletePetTask = _petService.RemovePetByIdAsync(inputId);
+        
+        // Assert
+        await Assert.ThrowsAsync<NotFoundPetException>(() => deletePetTask.AsTask());
+        
+        _loggingBrokerMock.Verify(broker =>
+            broker.LogError(It.Is(SameExceptionAs(expectedPetValidationException))),
+            Times.Once);
+        
+        _storageBrokerMock.Verify(broker =>
+            broker.SelectPetByIdAsync(inputId),
+            Times.Once);
+        
+        _dateTimeBrokerMock.VerifyNoOtherCalls();
+        _loggingBrokerMock.VerifyNoOtherCalls();
+        _storageBrokerMock.VerifyNoOtherCalls();
+    }
 }
