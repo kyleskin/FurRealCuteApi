905da297d4afcc1acbd28d204189118c78c7fb9c
Kyle Skinner <kyleskin@gmail.com>
2023-03-31 18:59:05 -0600

    ShouldThrowValidationExceptionOnRemoveWhenIdIsInvalidAndLogItAsync -> FAIL

---
+++ FurRealCute.Web.Api.Tests.Unit/Services/Pets/PetServiceTests.Validations.Remove.cs
@@ -0,0 +1,36 @@
+﻿using FluentAssertions;
+using FurRealCute.Web.Api.Models.Pets;
+using FurRealCute.Web.Api.Models.Pets.Exceptions;
+using Moq;
+
+namespace FurRealCute.Web.Api.Tests.Unit.Services.Pets;
+
+public partial class PetServiceTests
+{
+    [Fact]
+    public async Task ShouldThrowValidationExceptionOnRemoveWhenIdIsInvalidAndLogItAsync()
+    {
+        // Arrange
+        Guid invalidId = Guid.Empty;
+
+        InvalidPetException invalidPetException = new(
+            parameterName: nameof(Pet.Id),
+            parameterValue: invalidId);
+
+        PetValidationException expectedPetValidationException = new(invalidPetException);
+
+        // Act
+        ValueTask<Pet> removePetTask = _petService.RemovePetByIdAsync(invalidId);
+        
+        // Assert
+        await Assert.ThrowsAsync<PetValidationException>(() => removePetTask.AsTask());
+        
+        _loggingBrokerMock.Verify(broker =>
+            broker.LogError(It.Is(SameExceptionAs(expectedPetValidationException))),
+            Times.Once);
+        
+        _dateTimeBrokerMock.VerifyNoOtherCalls();
+        _loggingBrokerMock.VerifyNoOtherCalls();
+        _storageBrokerMock.VerifyNoOtherCalls();
+    }
+}
